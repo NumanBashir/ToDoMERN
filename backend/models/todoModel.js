6577520b4ac7dd580939f0182d58d42980473f90
NumanBashir <sportybashir@gmail.com>
2023-12-28 13:24:26 +0100

    Added todoModel

---
+++ backend/models/todoModel.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const todoSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const ToDo = mongoose.model("ToDo", todoSchema);
