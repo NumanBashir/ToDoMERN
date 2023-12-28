4724a1511cbfab7de71500059705c6b2c6347458
NumanBashir <sportybashir@gmail.com>
2023-12-28 13:50:50 +0100

    post a todo

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,35 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js ";
 import mongoose from "mongoose";
+import { ToDo } from "./models/todoModel.js";
 
 const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
 
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Welcome to ToDo MERN Stack Tutorial");
+});
+
+app.post("/todos", async (request, response) => {
+  try {
+    if (!request.body.title || !request.body.date) {
+      return response
+        .status(400)
+        .send({ message: "Send all required fields: title and date" });
+    }
+    const newTodo = {
+      title: request.body.title,
+      date: request.body.date,
+    };
+    const todo = await ToDo.create(newTodo);
+    return response.status(201).send(todo);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
