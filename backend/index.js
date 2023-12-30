9a4ea71c871fce9845b4a819f4a93a8321ac3fd9
NumanBashir <sportybashir@gmail.com>
2023-12-30 16:50:22 +0100

    route for updating a todo

--- backend/index.js
+++ backend/index.js
@@ -60,6 +60,28 @@ app.get("/todos/:id", async (request, response) => {
   }
 });
 
+// Route for update a todo
+app.put("/todos/:id", async (request, response) => {
+  try {
+    if (!request.body.title || !request.body.date) {
+      return response.status(400).send({
+        message: "Send all required fields: title and date",
+      });
+    }
+
+    const { id } = request.params;
+    const result = await ToDo.findByIdAndUpdate(id, request.body);
+    if (!result) {
+      return response.status(404).json({ message: "ToDo not found" });
+    }
+
+    return response.status(200).send({ message: "ToDo updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
