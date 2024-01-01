cacb30c181fffa944bd937bcfc9f6a1f4d6b8d65
NumanBashir <sportybashir@gmail.com>
2024-01-01 14:32:32 +0100

    Fix route error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CreateToDo from "./pages/CreateToDo";
 import ShowToDo from "./pages/ShowToDo";
